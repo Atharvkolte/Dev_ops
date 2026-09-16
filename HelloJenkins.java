83eb4a730878ba6253bc9c9d7bc816cf056d8821
Atharva kolte <atharv31kolte@gmail.com>
2026-09-16 19:32:24 +0530

    Create HelloJenkins.java

---
+++ HelloJenkins.java
@@ -0,0 +1,8 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Mayur");
+        System.out.println("Jenkins Java Build Lab");
+        System.out.println("Java program executed successfully.");
+        System.out.println("Build completed using Jenkins.");
+    }
+}
